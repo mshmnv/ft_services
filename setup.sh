fb880a17aea741c6e5b3c0db256b12ff57e62f8d
Mariq Minaewa <lbagg@at-e5.msk.21-school.ru>
2020-11-08 20:51:05 +0300

    start

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 minikube start
 
-# metalLB
+minikube addons enable metallb
+
 docker build -t nginx scrs/nginx
 docker build -t wordpress scrs/wordpress
 docker build -t php scrs/php
 docker build -t mariadb scrs/mysql
-docker build -t telegraf srcs/telegraf/  #why
+# docker build -t telegraf srcs/telegraf/  # why
 docker build -t grafana scrs/grafana
 docker build -t influxdb scrs/influxdb
 docker build -t ftps scrs/ftps
 
-
+minikube addons configure metallb  # in terminal: -- Enter Load Balancer Start IP:
 
 
