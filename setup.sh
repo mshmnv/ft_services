9e1d11d193c05ea6cd2d676d1180c859b1699e27
Mariq Minaewa <lbagg@su-f5.msk.21-school.ru>
2020-11-09 21:37:44 +0300

    some yaml

--- setup.sh
+++ setup.sh
@@ -1,23 +1,34 @@
+#!/bin/bash
+
 # создание kubernetes cluster
 minikube start # --vm-driver=virtualbox --cpus=2 --disk-size="10000mb" --memory="2000mb"
 
-
 minikube addons enable metallb
-minikube addons enable dashboard
+minikube addons enable dashboard # The Dashboard is a web-based Kubernetes user interface
 
 kubectl apply -f srcs/metallb-config.yaml
 
 docker build -t nginx scrs/nginx
-docker build -t wordpress scrs/wordpress
-docker build -t php scrs/php
-docker build -t mariadb scrs/mysql
-# docker build -t telegraf srcs/telegraf/  # why
-docker build -t grafana scrs/grafana
-docker build -t influxdb scrs/influxdb
-docker build -t ftps scrs/ftps
+# docker build -t wordpress scrs/wordpress
+# docker build -t php scrs/phpmyadmin
+# docker build -t mariadb scrs/mysql
+# docker build -t grafana scrs/grafana     
+# docker build -t influxdb scrs/influxdb   # time series db - база для хранения временных рядов
+# docker build -t ftps scrs/ftps
+# docker build -t telegraf srcs/telegraf/  # agent for collecting data
 
-# kubectl apply - для каждого в srcs
 
-minikube addons configure metallb  # in terminal: -- Enter Load Balancer Start IP:
+kubectl apply -f srcs/nginx-deployment.yaml
+# kubectl apply -f srcs/wordpress-deployment.yaml
+# kubectl apply -f srcs/phpmyadmin-deployment.yaml
+# kubectl apply -f srcs/mysql-deployment.yaml
+# kubectl apply -f srcs/influxdb-deployment.yaml
+# kubectl apply -f srcs/ftps-deployment.yaml
+# kubectl apply -f srcs/grafana-deployment.yaml
+
+
+
+
+# minikube addons configure metallb  # in terminal: -- Enter Load Balancer Start IP:
 
 
---
+++ srcs/nginx/start_nginx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rc-service nginx start
