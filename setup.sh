e93908932bcde6e1e9921da0af6bc51fc9ca4408
mshmnv <mshmnv@yandex.ru>
2020-11-09 14:40:20 +0300

    nginx start

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
-minikube start
+# создание kubernetes cluster
+minikube start # --vm-driver=virtualbox --cpus=2 --disk-size="10000mb" --memory="2000mb"
+
 
 minikube addons enable metallb
+minikube addons enable dashboard
+
+kubectl apply -f srcs/metallb-config.yaml
 
 docker build -t nginx scrs/nginx
 docker build -t wordpress scrs/wordpress
@@ -10,6 +15,8 @@ docker build -t mariadb scrs/mysql
 docker build -t grafana scrs/grafana
 docker build -t influxdb scrs/influxdb
 docker build -t ftps scrs/ftps
+
+# kubectl apply - для каждого в srcs
 
 minikube addons configure metallb  # in terminal: -- Enter Load Balancer Start IP:
 
